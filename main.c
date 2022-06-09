7c6408d5c1ff5d0ffc59761adcd81ab62ad3962f
Axis-Uz <76147986+Axis-Uz@users.noreply.github.com>
2022-06-09 13:09:58 +0530

    Add Semantics Function (Buggy) and some the other changes.

--- main.c
+++ main.c
@@ -4,84 +4,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "analysis.c"
+extern void semantics(char input_words[500][20], int num_of_words);
+extern void sentiment(char input_words[500][20], int num_of_words);
 
-int lineCount(int index) {
-	// To count the lines in the text file to create proper sized word arrays.
-	char temp_char;
-	FILE *file_pointer = NULL;
-	int num_of_lines = 0;
-	switch (index) {
-	case -2:
-		file_pointer = fopen("negative.txt", "r");
-		break;
-
-	case -1:
-		file_pointer = fopen("positive.txt", "r");
-		break;
-
-	case 0:
-		file_pointer = fopen("funwrd.txt", "r");
-		break;
-
-	case 1:
-		file_pointer = fopen("comp.txt", "r");
-		break;
-
-	case 2:
-		file_pointer = fopen("phy.txt", "r");
-		break;
-
-	case 3:
-		file_pointer = fopen("bio.txt", "r");
-		break;
-
-	case 4:
-		file_pointer = fopen("chemistry.txt", "r");
-		break;
-
-	default:
-		printf("\n!!<lineCount>--switch default hit");
-		break;
-	}
-
-	if (file_pointer == NULL) {
-		printf("\n!!<lineCount>--File missing");
-	} else {
-		temp_char = fgetc(file_pointer);
-		while (temp_char != EOF) {
-			if (temp_char == '\n') {
-				num_of_lines += 1;
-			}
-			temp_char = fgetc(file_pointer);
-		}
-	}
-	fclose(file_pointer);
-	/* [+1] for last line '\n' which doesn't exist,
-	another [+1] for array */
-	num_of_lines = num_of_lines + 2;
-	return num_of_lines;
-}
-
-void remove_symbols(char arr[]) {
+void remove_symbols(char arr[])
+{
 	// Use to replace symbols with a space from the arrays of char.
 
-	for (int i = 0; i < strlen(arr); i++) {
+	for (int i = 0; i < strlen(arr); i++)
+	{
 		// TODO Clean this if-else block
-		if ((0 <= arr[i] && arr[i] <= 47) || (58 <= arr[i] && arr[i] <= 64)) {
+		if ((0 <= arr[i] && arr[i] <= 47) || (58 <= arr[i] && arr[i] <= 64))
+		{
 			arr[i] = ' ';
-		} else if (
+		}
+		else if (
 			(91 <= arr[i] && arr[i] <= 96) ||
-			(123 <= arr[i] && arr[i] <= 127)) {
+			(123 <= arr[i] && arr[i] <= 127))
+		{
 			arr[i] = ' ';
-
-		} else if (arr[i] == '|') {
+		}
+		else if (arr[i] == '|')
+		{
 			// Can't remove "|" as we are using as stop symbol.
 			break;
 		}
 	}
 }
 
-int get_words(char arr[][20]) {
+int get_words(char arr[][20])
+{
 	char words[500][20]; //['word','hello']
 	int num_of_words = 0;
 
@@ -91,16 +44,19 @@ int get_words(char arr[][20]) {
 	printf("\n!! Enter words(to stop use 'space+|'): \n");
 	fflush(stdin);
 
-	for (int i = 0; i < 500; i++) {
+	for (int i = 0; i < 500; i++)
+	{
 		char letters[20]; // ['w','o','r','d']
 		scanf("%s", letters);
 		// If current inputted word is '|' then stop reading
-		if (*letters == '|') {
+		if (*letters == '|')
+		{
 			break;
 		}
 		remove_symbols(letters);
 		strcpy(words[i], letters);
-		for (int j = 0; j <= 10; j++) {
+		for (int j = 0; j <= 10; j++)
+		{
 			//? Why is "j<=10" ?
 			words[i][j] = tolower(words[i][j]);
 		}
@@ -111,26 +67,22 @@ int get_words(char arr[][20]) {
 	return num_of_words;
 }
 
-void semantics(char arr[500][20], int num_of_words) {
-	// Semantic Analysis.
-}
-void sentiment(char arr[500][20], int num_of_words) {
-	// Sentiment Analysis.
-}
-
-int main(void) {
+int main(void)
+{
 	char user_words[500][20];
 	int total_words, index, never = 0;
 
 	printf("\n\t<< Welcome >>");
 	printf("\n\t<< Semantic and Sentiment Analysis of Text >>");
 
-	while (never == 0) {
-		printf("\nAnalysis on:\n->1.Semantics\n->2.Setiment\n->3.Quit");
+	while (never == 0)
+	{
+		printf("\nAnalysis on:\n->1.Semantics\n->2.Sentiment\n->3.Quit");
 		printf("\nInput index number ->");
 		scanf("%d", &index);
 		sleep(1);
-		switch (index) {
+		switch (index)
+		{
 		case 1:
 
 			printf("\nCurrently semantics analysis is only limited for\n "
@@ -141,7 +93,7 @@ int main(void) {
 
 		case 2:
 			printf("\nCurrently sentiment analysis is only limited to "
-				   "Postivity and Negativity.\n");
+				   "Positivity and Negativity.\n");
 			total_words = get_words(user_words);
 			sentiment(user_words, total_words);
 			break;
