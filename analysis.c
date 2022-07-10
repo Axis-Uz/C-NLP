cb8383a837d0c1b3517c3194f1c4724998c6a9ae
Axis-Uz <76147986+Axis-Uz@users.noreply.github.com>
2022-07-10 20:43:41 +0530

    Updated lineCount function and removed exclusion factor from readArr.

--- analysis.c
+++ analysis.c
@@ -4,50 +4,15 @@
 #include <ctype.h>
 #include <unistd.h>
 
-int lineCount(int index)
+int lineCount(char *file_name)
 {
-    // Count the number of lines in the file.
+    // Count the number of line in files.
     char temp;
-    FILE *file_pointer;
-    int num_of_line = 0;
-    switch (index)
-    {
-    case -2:
-        file_pointer = fopen("negative.csv", "r");
-        break;
-
-    case -1:
-        file_pointer = fopen("positive.csv", "r");
-        break;
-
-    case 0:
-        file_pointer = fopen("fun.csv", "r");
-        break;
-
-    case 1:
-        file_pointer = fopen("comp.csv", "r");
-        break;
-
-    case 2:
-        file_pointer = fopen("phy.csv", "r");
-        break;
-
-    case 3:
-        file_pointer = fopen("bio.csv", "r");
-        break;
-
-    case 4:
-        file_pointer = fopen("chem.csv", "r");
-        break;
-
-    default:
-        printf("\n!!(analysis.c)<lineCount>--Switch is Default(%d)", index);
-        break;
-    }
-
+    int num_of_line;
+    FILE *file_pointer = fopen(file_name, "r");
     if (file_pointer == NULL)
     {
-        printf("\n!!(analysis.c)<lineCount>--File is Missing(%d)", index);
+        printf("\n!!(analysis.c)<lineCount>--File is Missing.");
     }
     else
     {
@@ -63,11 +28,7 @@ int lineCount(int index)
     }
 
     fclose(file_pointer);
-
-    /* +1 for last line i.e '\n' and another +1 for array size*/
-    num_of_line += 2;
-
-    return num_of_line;
+    return num_of_line + 2;
 }
 
 void readFile(char *file_name, int end_line, char arr_words[][20])
@@ -77,7 +38,7 @@ void readFile(char *file_name, int end_line, char arr_words[][20])
     FILE *file_pointer = fopen(file_name, "r");
     if (file_pointer == NULL)
     {
-        printf("\n!!(analysis.c)<read_arr>--File is Missing.");
+        printf("\n!!(analysis.c)<readFile>--File is Missing.");
     }
     else
     {
@@ -90,26 +51,19 @@ void readFile(char *file_name, int end_line, char arr_words[][20])
     fclose(file_pointer);
 }
 
-int compareArr(char arr[][20], int end_line, char input[][20], int total_words, int exclusion_list[])
+int compareArr(char arr[][20], int end_line, char input[][20], int total_words)
 {
     // It compare second array with first array and return the number of same words in both array.
     int compare;
     int result = 0;
     for (int i = 0; i < total_words; i++)
     {
-        if (exclusion_list[i] == 1)
-        {
-            /* Excludes already compared words */
-            // printf("\n%d\t%s--Excluded", i, input[i]);
-            continue;
-        }
         for (int loop = 0; loop < end_line; loop++)
         {
             compare = strcmp(input[i], arr[loop]);
             if (compare == 0)
             {
                 result += 1;
-                exclusion_list[i] = 1;
             }
         }
     }
@@ -118,17 +72,17 @@ int compareArr(char arr[][20], int end_line, char input[][20], int total_words, 
 
 void semantics(char user_words[500][20], int num_of_words)
 {
-
+    // Semantics analysis of text, categorizing text according the topic.
     int result[5] = {0, 0, 0, 0, 0},
         unkown_words, exclusion[500];
     float unkown_percent, percent[5] = {0, 0, 0, 0, 0};
 
     int fun_line, comp_line, phy_line, bio_line, chem_line;
-    fun_line = lineCount(0);
-    comp_line = lineCount(1);
-    phy_line = lineCount(2);
-    bio_line = lineCount(3);
-    chem_line = lineCount(4);
+    fun_line = lineCount("fun.csv");
+    comp_line = lineCount("comp.csv");
+    phy_line = lineCount("phy.csv");
+    bio_line = lineCount("bio.csv");
+    chem_line = lineCount("chem.csv");
     char fun_words[fun_line][20], comp_words[comp_line][20],
         phy_words[phy_line][20], bio_words[bio_line][20], chem_words[chem_line][20];
 
@@ -138,11 +92,11 @@ void semantics(char user_words[500][20], int num_of_words)
     readFile("bio.csv", bio_line, bio_words);
     readFile("chem.csv", chem_line, chem_words);
 
-    result[0] = compareArr(fun_words, fun_line, user_words, num_of_words, exclusion);
-    result[1] = compareArr(comp_words, comp_line, user_words, num_of_words, exclusion);
-    result[2] = compareArr(phy_words, phy_line, user_words, num_of_words, exclusion);
-    result[3] = compareArr(bio_words, bio_line, user_words, num_of_words, exclusion);
-    result[4] = compareArr(chem_words, chem_line, user_words, num_of_words, exclusion);
+    result[0] = compareArr(fun_words, fun_line, user_words, num_of_words);
+    result[1] = compareArr(comp_words, comp_line, user_words, num_of_words);
+    result[2] = compareArr(phy_words, phy_line, user_words, num_of_words);
+    result[3] = compareArr(bio_words, bio_line, user_words, num_of_words);
+    result[4] = compareArr(chem_words, chem_line, user_words, num_of_words);
 
     for (int i = 0; i < 5; i++)
     {
@@ -158,7 +112,7 @@ void semantics(char user_words[500][20], int num_of_words)
     printf("\n  Biology     : %.2f%% (%d)", percent[3], result[3]);
     printf("\n  Chemistry   : %.2f%% (%d)", percent[4], result[4]);
     printf("\n  Unknown     : %.2f%% (%d)", unkown_percent, unkown_words);
-    printf("\n  Total Words : %d",num_of_words);
+    printf("\n  Total Words : %d", num_of_words);
 
     printf("\n The sentence seems to be related to: ");
     if (percent[1] > percent[2] && percent[1] > percent[3] && percent[1] > percent[4])
